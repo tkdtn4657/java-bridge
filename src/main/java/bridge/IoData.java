6247846e8b6949ddf30ed15c1775175185f30349
tkdtn4657 <tkdtn101226@gmail.com>
2022-11-17 15:14:40 +0900

    PrintMessage를 사용하는 출력클래스 IoData구현

--- src/main/java/bridge/IoData.java
+++ src/main/java/bridge/IoData.java
@@ -1,9 +1,45 @@
 package bridge;
 
+
+
 public class IoData {
+	String message;
 	
-	IoData(){
-		
+	IoData(PrintMessage tmp){
+		this.message = tmp.getName();
+	}
+	static void outputGoGame(){
+		IoData output = new IoData(PrintMessage.PRINT_OUTPUT_GOGAME);
+		System.out.println(output.message);
+	}
+	
+	static void inputSize(){
+		IoData output = new IoData(PrintMessage.PRINT_INPUT_SIZE);
+		System.out.println(output.message);
+	}
+	
+	static void inputUpDown(){
+		IoData output = new IoData(PrintMessage.PRINT_INPUT_UPDOWN);
+		System.out.println(output.message);
+	}
+	
+	static void inputRetry(){
+		IoData output = new IoData(PrintMessage.PRINT_INPUT_RETRY);
+		System.out.println(output.message);
+	}
+	
+	static void outputResult(){
+		IoData output = new IoData(PrintMessage.PRINT_OUTPUT_RESULT);
+		System.out.println(output.message);
+	}
+	
+	static void outputGameComplite(){
+		IoData output = new IoData(PrintMessage.PRINT_OUTPUT_GAMECOMPLITE);
+		System.out.println(output.message);
+	}
+	static void outputTryCount(){
+		IoData output = new IoData(PrintMessage.PRINT_OUTPUT_TRYCOUNT);
+		System.out.println(output.message);
 	}
 	
 	
--- src/main/java/bridge/PrintMessage.java
+++ src/main/java/bridge/PrintMessage.java
@@ -2,13 +2,13 @@ package bridge;
 
 public enum PrintMessage{
 	
-	OUTPUT_GOGAME("다리 건너기 게임을 시작합니다."),
-	INPUT_SIZE("다리의 길이를 입력해주세요."),
-	INPUT_UPDOWN("이동할 칸을 선택해주세요. (위: U, 아래: D"),
-	INPUT_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q"),
-	OUTPUT_RESULT("최종 게임 결과"),
-	OUTPUT_GAMECOMPLITE("게임 성공 여부: "),
-	OUTPUT_TRYCOUNT("총 시도한 횟수: ");
+	PRINT_OUTPUT_GOGAME("다리 건너기 게임을 시작합니다."),
+	PRINT_INPUT_SIZE("다리의 길이를 입력해주세요."),
+	PRINT_INPUT_UPDOWN("이동할 칸을 선택해주세요. (위: U, 아래: D"),
+	PRINT_INPUT_RETRY("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q"),
+	PRINT_OUTPUT_RESULT("최종 게임 결과"),
+	PRINT_OUTPUT_GAMECOMPLITE("게임 성공 여부: "),
+	PRINT_OUTPUT_TRYCOUNT("총 시도한 횟수: ");
 
 	private final String message;
 	
